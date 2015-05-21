31d98804c73bce0a0f8f46eb71ed775b95988270
Harold Teunisse <geek4law@gmail.com>
2015-05-21 12:14:26 -0700

    Update event_factory add event class

--- test/factories/event_factory.rb
+++ test/factories/event_factory.rb
@@ -1,19 +1,25 @@
-FactoryGirl.define do 
+FactoryGirl.define do
   factory :event do
-    
-    date {(Date.today - Faker::Number.number(3).to_i).strftime("%d/%m/%y")}
+
+    class Event
+      attr_accessor :type, :date, :amount, :location, :notes
+    end
+
+  #  skip_create
+
+    date {(Date.today - Faker::Number.number(3).to_i).strftime("%m/%d/%y")}
     amount {Faker::Number.number(1)}
-    location {Faker::Business.bs}
-    notes  {Faker::Lorem.sentence}   
-    
+    location {Faker::Company.bs}
+    notes  {Faker::Lorem.sentence}
+
     trait :camping do
       type 'camping'
     end
-    
-    trait :hiking do 
+
+    trait :hiking do
       type 'hiking'
     end
-    
+
     trait :service do
       type 'service'
     end
