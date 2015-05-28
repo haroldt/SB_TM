96dd50ad1c41009e8c9a4aa50dd90474e65b6242
Harold Teunisse <geek4law@gmail.com>
2015-05-28 14:28:39 -0700

    Add anonimized Test Fixtures

---
+++ lib/helper.rb
@@ -0,0 +1,17 @@
+require 'TM_Log.rb'
+
+def anonimize_data(record)
+
+  name_loc = record.index("Name:")+1
+  phone_loc = record.index("Home Phone:")+1
+  dob_loc = record.index("Date of Birth:")+1
+  age_loc = record.index("Age:")+1
+
+  age = record[age_loc].to_i
+  record[dob_loc] = Faker::Date.between((age+1).years.ago, age.years.ago)
+  record[name_loc] = Faker::Name.name.split[0..1].reverse.join(',')
+  record[phone_loc] = Faker::PhoneNumber.cell_phone
+
+  record
+
+end
