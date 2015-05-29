f1b97ef02bd46a007102aaa145c4215b9b209a35
Harold Teunisse <geek4law@gmail.com>
2015-05-28 18:57:40 -0700

    format date as a string

--- lib/helper.rb
+++ lib/helper.rb
@@ -8,7 +8,7 @@ def anonimize_data(record)
   age_loc = record.index("Age:")+1
 
   age = record[age_loc].to_i
-  record[dob_loc] = Faker::Date.between((age+1).years.ago, age.years.ago)
+  record[dob_loc] = Faker::Date.between((age+1).years.ago, age.years.ago).strftime('%m/%d/%y')
   record[name_loc] = Faker::Name.name.split[0..1].reverse.join(',')
   record[phone_loc] = Faker::PhoneNumber.cell_phone
 
